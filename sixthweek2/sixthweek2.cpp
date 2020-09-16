388903eecc5c38bbb30eaf5dec1833874eb45f12
iamsurapat12 <iamsurapat12@outlook.co.th>
2020-09-16 17:01:10 +0700

    Add project files.

---
+++ sixthweek2/sixthweek2.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void Rhombus(int n)
+{
+	int x = 1;
+	while (x <= n - 1)
+	{
+		int a = 1;
+		while (a <= n - x)
+		{
+			printf(" ");
+			a++;
+		}
+		int b = 1;
+		while (b <= 2 * x - 1)
+		{
+			printf("*");
+			b++;
+		}
+		x++;
+
+		printf("\n");
+	}
+	int y = 1;
+	while (y <= 2 * n - 1)
+	{
+		printf("*");
+		y++;
+	}
+	printf("\n");
+	int z = 1;
+	while (z <= n - 1)
+	{
+		int c = 1;
+		while (c <= z)
+		{
+			printf(" ");
+			c++;
+		}
+		int d = 1;
+		while (d <= 2 * n - 1 - 2 * z)
+		{
+			printf("*");
+			d++;
+		}
+		z++;
+		printf("\n");
+	}
+}
+int main()
+{
+	int x;
+	printf("Enter the number : ");
+	scanf_s("%d", &x);
+	Rhombus(x);
+
+	return 0;
+}
